6499ebadc4e4796d693bd3cac8f5c4861f458618
AndreaRomano02 <andy.romano2002@gmail.com>
2023-06-14 17:59:45 +0200

    Add another method to do a third snack

--- js/Snack3.js
+++ js/Snack3.js
@@ -27,9 +27,9 @@ const bikes = [
 ];
 
 //# Recupero il peso delle biciclette dall'array
-const minWeight = bikes.reduce((prev, curr) =>
-  prev.weight < curr.weight ? prev : curr
-);
+const minWeight = bikes.reduce((prev, curr) => {
+  return prev.weight < curr.weight ? prev : curr;
+}, bikes[0]);
 
 console.log(minWeight);
 
@@ -37,4 +37,19 @@ dom.innerHTML = `<div class="m-3">
   <h1>SNACK 3</h1>
   Il nome della bici che pesa meno è: ${minWeight.name} <br>
   Il peso è: ${minWeight.weight}Kg
+</div>`;
+
+let minWeight2 = bikes[0];
+
+for (let bike of bikes) if (bike.weight < minWeight2.weight) minWeight2 = bike;
+
+console.log(minWeight2);
+
+//# Destructuring di 'minWeight2'
+const { name, weight } = minWeight2;
+
+dom.innerHTML += `<div class="m-3">
+  <h1>SNACK 3 Secondo metodo</h1>
+  Il nome della bici che pesa meno è: ${name} <br>
+  Il peso è: ${weight}Kg
 </div>`;
